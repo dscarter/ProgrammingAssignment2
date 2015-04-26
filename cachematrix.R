2dd3d3d20b20a074fa900a495612ea51c9167c0c
dscarter <dscarter@uwalumni.com>
2015-04-26 00:22:28 -0500

    Committing changes that create the cache functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,51 @@
 ## Put comments here that give an overall description of what your
 ## functions do
+## makeCacheMatrix creates a list of functions.
+##	the functions can set a matrix to store,
+##	return that matrix, calculate the inverse of the matrix
+##	and also return that matrix inverse
+## cacheSolve checks if the inverse has already been calculated
+##	and returns it if already done.  If it has not been done
+##	then it calculates the inverse and stores it with 
+##	makeCacheMatrix
+##	
+
 
 ## Write a short comment describing this function
+##	setMat sets the matrix and clears out the cached inv
+##      getMat returns the cached matrix
+##      setinvMat stores an inverse matrix
+##      getinvMat returns the cached inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        invMat <- NULL
+        setMat <- function(y) {
+                x <<- y
+                invMat <<- NULL
+        }
+        getMat <- function() x
+        setinvMat <- function(inv) invMat <<- inv
+        getinvMat <- function() invMat
+        list(setMat=setMat,getMat=getMat
+             ,setinvMat=setinvMat,getinvMat=getinvMat)
+        
 }
 
 
 ## Write a short comment describing this function
+##      Checks to see if the inverse of a matrix has already been
+##      solved.  If it has then it returns it.  If it has not then
+##      it calculates it.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        MatrixInverse <- x$getinvMat()  #set the inverse from the cached matrix 
+        if(!is.null(MatrixInverse)) {   #if the cached inverse is not empty then return it
+                message("getting cached data")
+                return(MatrixInverse)
+        }
+        
+        MatrixInverse <- solve(x$getMat(),...)  #inverse was not calculated so do so now
+        x$setinvMat(MatrixInverse)
+        MatrixInverse
 }
